bc8b09e14b9b594c7b3df2c212f28eef47fba6c4
«ALIAKSEI <alexditarget@gmail.com>
2022-07-23 00:30:40 +0300

    upgrade finish 22.07.2022

--- behance_helper_bd.py
+++ behance_helper_bd.py
@@ -13,3 +13,24 @@ def connect_database(id, url):
                 connection.commit()
     except Error as error:
         print(error)
+
+
+
+# def get_url_history():
+#     try:
+#         with connect(host='31.31.196.38', user='u1726449_alex', password='vY9aQ3gX3x') as connection:
+#             print(connection)
+#             request_mysql = 'USE u1726449_default'
+#             request_select = "SELECT url_interface, id FROM behance_helper WHERE client_id=1172947980"
+#             print(request_select)
+#             with connection.cursor() as cursor:
+#                 cursor.execute(request_mysql)
+#                 cursor.execute(request_select)
+#                 result = cursor.fetchall()
+#                 for i in result:
+#                     print(i[0])
+#                 connection.commit()
+#     except Error as error:
+#         print(error)
+#
+# get_url_history()

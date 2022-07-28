986722e3d03c376b75c09d18f494ce7622723fb5
«ALIAKSEI <alexditarget@gmail.com>
2022-07-28 12:54:39 +0300

    upgrade DataBase class

--- database.py
+++ database.py
@@ -6,6 +6,9 @@ class DataBase:
 
     connection = None
     cursor = None
+    request_data_insert = None
+    request_reading_last_note = None
+    request_reading_history = None
 
     def __init__(self, host, user, password, database):
         """Initialisation of Class Object."""
@@ -26,45 +29,51 @@ class DataBase:
         """Cursor definition."""
         self.cursor = self.connection.cursor()
 
-
-class DataBaseAction(DataBase):
-    """Database management class."""
-
-    request_insert = None
-    request_reading = None
-
-    def __init__(self, host, user, password, database, id, url):
-        """Initialisation of Class Object."""
-        super().__init__(host, user, password, database)
-        self.id = id
-        self.url = url
-
-    def insert_data(self):
+    def insert_data(self, id, url):
         """Insert data into database."""
-        self.request_insert = f'''
+        self.request_data_insert = f'''
         INSERT INTO behance_helper (
             client_id,
             url_interface)
-        VALUES ('{self.id}', '{self.url}')
+        VALUES ('{id}', '{url}')
         '''
         try:
-            self.cursor.execute(self.request_insert)
+            self.cursor.execute(self.request_data_insert)
             self.connection.commit()
         except Error as error:
             print(error)
 
-    def reading_data(self):
+    def reading_last_note(self, id):
+        """Return last note from database by client_id."""
+        self.request_reading_last_note = f'''
+        SELECT
+            url_interface, id
+        FROM
+            behance_helper
+        WHERE client_id = '{id}'
+        ORDER BY id
+        DESC LIMIT 1
+        '''
+        try:
+            self.cursor.execute(self.request_reading_last_note)
+            result = self.cursor.fetchall()
+            self.connection.commit()
+            return result[0][0].lower()
+        except Error as error:
+            print(error)
+
+    def reading_history(self, id):
         """Return info (set urls) from database."""
-        self.request_reading = f'''
+        self.request_reading_history = f'''
         SELECT
             url_interface, id
         FROM
             behance_helper
         WHERE
-            client_id='{str(self.id)}'
+            client_id='{id}'
         '''
         try:
-            self.cursor.execute(self.request_reading)
+            self.cursor.execute(self.request_reading_history)
             result = self.cursor.fetchall()
             self.connection.commit()
             result_string = ' '.join(list(set([i[0].lower() for i in result])))

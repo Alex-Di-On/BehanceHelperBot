e6de40fa8d5df6da446b8eb7972b88ab11b6224e
«ALIAKSEI <alexditarget@gmail.com>
2022-07-31 22:37:18 +0300

    add TelegramAPI class

--- telegram.py
+++ telegram.py
@@ -8,6 +8,8 @@ class TelegramAPI:
 
     URL = 'https://api.telegram.org/bot'
     TOKEN = configuration['token']
+    client_id = None
+    message = None
 
     def __init__(self, identification: int):
         """Initialisation of Class Object."""
@@ -17,22 +19,16 @@ class TelegramAPI:
         """Return POST-request."""
         return requests.post(self.URL + self.TOKEN + method, data=body)
 
-    def get_update(self) -> requests.models.Response:
-        """Return Response from Telegram."""
+    def get_info(self) -> None:
+        """Getting info about Client."""
         data = {'offset': self.identification, 'limit': 1, 'timeout': 0}
-        return self.get_post_request('/getUpdates', data)
-
-    def get_client_id(self) -> int:
-        """Return Client id."""
-        return self.get_update().json()['result'][0]['message']['from']['id']
-
-    def get_message(self) -> str:
-        """Return text message from Client."""
-        return self.get_update().json()['result'][0]['message']['text']
+        response = self.get_post_request('/getUpdates', data)
+        self.client_id = response.json()['result'][0]['message']['from']['id']
+        self.message = response.json()['result'][0]['message']['text']
 
     def send_message(self, text: str, command: str = None, button: list = None) -> None:
         """Sending message to Client."""
-        data = {'chat_id': self.get_client_id(), 'text': text}
+        data = {'chat_id': self.client_id, 'text': text}
         match command:
             case 'set_buttons':
                 data['reply_markup'] = json.dumps({'keyboard': button, 'one_time_keyboard': False})

7e7af0a7cf0d1ec61526c5ae653deac92d675cb0
«ALIAKSEI <alexditarget@gmail.com>
2022-08-04 11:13:22 +0300

    upgrade main.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-
-while True:
-    num = int(input())
-    if num % 7 == 0:
-        continue
-    print(num)
-
-
-    
+#
+# while True:
+#     num = int(input())
+#     if num % 7 == 0:
+#         continue
+#     print(num)
+#
+#
+# a = ''
+#
+# if not a:
+#     print('пустая')
+# else:
+#     print('не пустая')
+#

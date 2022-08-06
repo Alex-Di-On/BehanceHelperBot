30e08a7b0158b1167c4f0ba3088919374d68b9bf
«ALIAKSEI <alexditarget@gmail.com>
2022-08-06 08:12:15 +0300

    upgrade main.py

--- main.py
+++ main.py
@@ -73,17 +73,18 @@ if __name__ == '__main__':
                         try:
                             database.call_database('last_note', bot.client_id)
                             url = ParserBehance(database.result[0][0].lower())
+                            name = url.user_name.capitalize()
                             if bot.message == "Author's country":
                                 country = country_filter(url.get_country())
                                 flag = f"{emojize(f':{country}:')}"
-                                bot.send_message(f'Country of {url.user_name} is {country} {flag}')
+                                bot.send_message(f'Country of {name} is {country} {flag}')
                             else:
                                 key = bot.message[9:].title()
                                 try:
                                     value = url.get_statistics()[key]
                                 except KeyError:
                                     value = url.statistics[key]
-                                bot.send_message(f'{key.title()} of {url.user_name} is {value}.')
+                                bot.send_message(f'{key} of {name} is {value}.')
                         except IndexError:
                             bot.send_message(answers['no_history'])
                     case _:

d28bb4986a6f9b99e290ea1a30470d43c9bb0508
«ALIAKSEI <alexditarget@gmail.com>
2022-08-13 13:34:32 +0300

    upgrade main.py

--- main.py
+++ main.py
@@ -43,71 +43,65 @@ if __name__ == '__main__':
     print('Connection to DataBase is successful.')
     while True:
         time.sleep(0.5)
-
-
-
-
-        try:
-            bot = TelegramAPI(update_id)
-            bot.get_info()
-        except IndexError:
-            print(answers['nobody_texted'])
-            continue
-        if database.check_connection():
-            if language_test(bot.message):
-                match bot.message:
-                    case '/start' | 'CHANGE URL':
-                        bot.send_message(answers['start'], 'del_buttons')
-                    case 'REQUEST HISTORY':
-                        database.call_database('history', bot.client_id)
-                        history_result = ', '.join(list(set([i[0].lower() for i in database.result])))
-                        if not history_result:
-                            bot.send_message(answers['empty_history'])
-                        else:
-                            bot.send_message(f"{answers['request_history']} {history_result}.")
-                    case "Author's project views" | "Author's appreciations" |\
-                         "Author's followers" | "Author's following" | "Author's country":
-                        try:
-                            database.call_database('last_note', bot.client_id)
-                            url = ParserBehance(database.result[0][0].lower())
-                            name = url.user_name.capitalize()
-                            if bot.message == "Author's country":
-                                country = country_filter(url.get_country())
-                                flag = f"{emojize(f':{country}:')}"
-                                bot.send_message(f'Country of {name} is {country} {flag}')
-                            else:
-                                key = bot.message[9:].title()
-                                try:
-                                    value = url.get_statistics()[key]
-                                except KeyError:
-                                    value = url.statistics[key]
-                                bot.send_message(f'{key} of {name} is {value}.')
-                        except IndexError:
-                            bot.send_message(answers['no_history'])
-                    case _:
-                        author = ParserBehance(bot.message)
-                        if author.url_validation():
-                            database.call_database('insert', bot.client_id, bot.message)
-                            bot.send_message(answers['menu'], 'set_buttons', buttons_menu)
-                        else:
-                            bot.send_message(answers['no_portfolio'])
-            else:
-                bot.send_message(answers['language_test'])
+        bot = TelegramAPI(update_id)
+        if bot.get_info_dict()['text'] is None:
+            print('Nobody texted to Bot.')
         else:
-            bot.send_message(answers['error_db'])
-            try:
-                smtp_object = SMTP(configuration['system_domen'], port=configuration['port'])
-                smtp_object.starttls()
-                smtp_object.login(user=configuration['system_mail'], password=configuration['system_mail_password'])
-                message = MIMEMultipart()
-                message['From'] = configuration['system_mail']
-                message['To'] = configuration['admin_mail']
-                message['Subject'] = answers['mail_error_subject']
-                message.attach(MIMEText(answers['warning_mail']))
-                smtp_object.sendmail(message['From'], message['To'], message.as_string())
-                smtp_object.quit()
-            except SMTPAuthenticationError:
-                admin_message({'chat_id': configuration['admin_id'], 'text': answers['error_auth']})
-            except SMTPConnectError:
-                admin_message({'chat_id': configuration['admin_id'], 'text': answers['error_connect']})
-        update_id += 1
+            if database.check_connection():
+                if language_test(bot.message):
+                    match bot.message:
+                        case '/start' | 'CHANGE URL':
+                            bot.send_message(answers['start'], 'del_buttons')
+                        case 'REQUEST HISTORY':
+                            database.call_database('history', bot.client_id)
+                            history_result = ', '.join(list(set([i[0].lower() for i in database.result])))
+                            if not history_result:
+                                bot.send_message(answers['empty_history'])
+                            else:
+                                bot.send_message(f"{answers['request_history']} {history_result}.")
+                        case "Author's project views" | "Author's appreciations" |\
+                             "Author's followers" | "Author's following" | "Author's country":
+                            try:
+                                database.call_database('last_note', bot.client_id)
+                                url = ParserBehance(database.result[0][0].lower())
+                                name = url.user_name.capitalize()
+                                if bot.message == "Author's country":
+                                    country = country_filter(url.get_country())
+                                    flag = f"{emojize(f':{country}:')}"
+                                    bot.send_message(f'Country of {name} is {country} {flag}')
+                                else:
+                                    key = bot.message[9:].title()
+                                    try:
+                                        value = url.get_statistics()[key]
+                                    except KeyError:
+                                        value = url.statistics[key]
+                                    bot.send_message(f'{key} of {name} is {value}.')
+                            except IndexError:
+                                bot.send_message(answers['no_history'])
+                        case _:
+                            author = ParserBehance(bot.message)
+                            if author.url_validation():
+                                database.call_database('insert', bot.client_id, bot.message)
+                                bot.send_message(answers['menu'], 'set_buttons', buttons_menu)
+                            else:
+                                bot.send_message(answers['no_portfolio'])
+                else:
+                    bot.send_message(answers['language_test'])
+            else:
+                bot.send_message(answers['error_db'])
+                try:
+                    smtp_object = SMTP(configuration['system_domen'], port=configuration['port'])
+                    smtp_object.starttls()
+                    smtp_object.login(user=configuration['system_mail'], password=configuration['system_mail_password'])
+                    message = MIMEMultipart()
+                    message['From'] = configuration['system_mail']
+                    message['To'] = configuration['admin_mail']
+                    message['Subject'] = answers['mail_error_subject']
+                    message.attach(MIMEText(answers['warning_mail']))
+                    smtp_object.sendmail(message['From'], message['To'], message.as_string())
+                    smtp_object.quit()
+                except SMTPAuthenticationError:
+                    admin_message({'chat_id': configuration['admin_id'], 'text': answers['error_auth']})
+                except SMTPConnectError:
+                    admin_message({'chat_id': configuration['admin_id'], 'text': answers['error_connect']})
+            update_id += 1

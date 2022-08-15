7b88a0a1eb32a04b27ca93290609cbf494dbab7a
«ALIAKSEI <alexditarget@gmail.com>
2022-08-15 18:15:29 +0300

    upgrade main.py

--- helper_box.py
+++ helper_box.py
@@ -12,8 +12,9 @@ def admin_email() -> None:
     message = MIMEMultipart()
     message['From'] = configuration['bot_mail']
     message['To'] = configuration['admin_mail']
-    message['Subject'] = answers['mail_error_subject']
-    message.attach(MIMEText(answers['warning_mail']))
+    message['Subject'] = 'Warning! DataBase connection error!'
+    message.attach(MIMEText('Warning! There is a problem with DataBase connection for BehanceHelperBot. '
+                            'Check functionality of DataBase on REG.RU, please.'))
     smtp_object.sendmail(message['From'], message['To'], message.as_string())
     smtp_object.quit()
 
@@ -43,13 +44,7 @@ bot_answers = {
     'menu': 'What info would you like to receive?',
     'no_portfolio': "Author hasn't portfolio on Behance.",
     'language_test': "I don't understand you. Use English, please.",
-    'no_country': "Author didn't indicate country on the form.",
     'error_db': "Sorry, I can't answer you at the moment. Please, try again later.",
     'empty_history': 'Your request history is empty.',
-    'no_history': "Sorry, I don't see your request history. Please, input author's URL on Behance again.",
-    'request_history': 'YOUR REQUEST HISTORY:',
-    'telegram_error': 'Failed to connect to Telegram server.',
-    'mail_error_subject': 'Warning! DataBase connection error!',
-    'warning_mail': 'Warning! There is a problem with DataBase connection for BehanceHelperBot. '
-                    'Check functionality of DataBase on REG.RU, please.'
+    'no_history': "Sorry, I don't see your request history. Please, input author's URL on Behance again."
 }
